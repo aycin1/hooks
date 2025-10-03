5ebc3a5de1e41ef89cb52382c9fa8bcb050f452a
aycin <aycinkaya98@gmail.com>
2025-10-03 11:56:03 +0100

    added react checkbox tree to attributes component (which receives pattern attributes via third party API) to send the options selected to RefineSearch)

--- src/pages/Search/components/Attributes.jsx
+++ src/pages/Search/components/Attributes.jsx
@@ -1,16 +1,16 @@
-// import {
-//   faChevronDown,
-//   faChevronRight,
-//   faFolder,
-//   faFolderOpen,
-//   faMinusSquare,
-//   faPlusSquare,
-//   faSquare,
-//   faSquareCheck,
-// } from "@fortawesome/free-solid-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronDown,
+  faChevronRight,
+  faFolder,
+  faFolderOpen,
+  faMinusSquare,
+  faPlusSquare,
+  faSquare,
+  faSquareCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
-// import CheckboxTree from "react-checkbox-tree";
+import CheckboxTree from "react-checkbox-tree";
 import "react-checkbox-tree/lib/react-checkbox-tree.css";
 import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
 import styles from "../Search.module.css";
@@ -40,76 +40,69 @@ export default function Attributes({ attChecked, setAttChecked }) {
     };
   }, []);
 
-  function mapAttributes(attribute) {
-    return attribute?.pattern_attributes?.map((attribute) => {
-      return (
-        <div>
-          <input type="checkbox" id={attribute.id} />
-          <label htmlFor={attribute.id}>{attribute.name}</label>
-        </div>
-      );
-    });
-  }
-
   function sortAttributes(attributes) {
-    if (attributes?.length) {
-      return attributes.map((attribute) => {
-        return (
-          <>
-            <h4>{attribute.name}</h4>
-            {attribute.pattern_attributes[0].name === "placeholder" ? (
-              sortAttributes(attribute.children)
-            ) : attribute.children?.length ? (
-              <>
-                {mapAttributes(attribute)}
-                {sortAttributes(attribute.children)}
-              </>
-            ) : (
-              mapAttributes(attribute)
-            )}
-          </>
-        );
-      });
-    }
+    return attributes?.map((attributeObj, index) => {
+      attributeObj.value = attributeObj.permalink || attributeObj.id;
+      attributeObj.label = attributeObj.name;
+      if (attributeObj?.pattern_attributes?.[0]?.name === "placeholder") {
+        delete attributeObj.pattern_attributes;
+        attributes.splice(index, 1);
+        attributeObj?.children?.map((child) => {
+          return attributes.unshift(child);
+        });
+      } else if (
+        !attributeObj?.children?.length &&
+        attributeObj?.pattern_attributes
+      ) {
+        attributeObj.children = attributeObj?.pattern_attributes;
+        delete attributeObj.pattern_attributes;
+        sortAttributes(attributeObj.children);
+      } else if (attributeObj?.children) {
+        const newChildren = attributeObj.children;
+        attributeObj?.pattern_attributes?.map((att) => {
+          return newChildren.push(att);
+        });
+        attributeObj.children = newChildren;
+        delete attributeObj.pattern_attributes;
+        sortAttributes(attributeObj.children);
+      }
+      return attributeObj;
+    });
   }
 
   return (
     <div className={styles.attributes}>
       <h2>Attributes</h2>
-      {
-        attributes && console.log(attributes)
-        // (
-        //   <CheckboxTree
-        //     nodes={sortAttributes(attributes)}
-        //     onlyLeafCheckboxes={true}
-        //     checked={attChecked}
-        //     expanded={expanded}
-        //     onCheck={(checked) => setAttChecked(checked)}
-        //     onExpand={(expanded) => setExpanded(expanded)}
-        //     icons={{
-        //       check: (
-        //         <FontAwesomeIcon
-        //           icon={faSquareCheck}
-        //           style={{ color: "#709c62ff" }}
-        //         />
-        //       ),
-        //       uncheck: (
-        //         <FontAwesomeIcon icon={faSquare} style={{ color: "#709c62ff" }} />
-        //       ),
-        //       expandClose: (
-        //         <FontAwesomeIcon icon={faChevronRight} style={style} />
-        //       ),
-        //       expandOpen: <FontAwesomeIcon icon={faChevronDown} style={style} />,
-        //       expandAll: <FontAwesomeIcon icon={faPlusSquare} style={style} />,
-        //       collapseAll: <FontAwesomeIcon icon={faMinusSquare} style={style} />,
-        //       parentClose: <FontAwesomeIcon icon={faFolder} style={style} />,
-        //       parentOpen: <FontAwesomeIcon icon={faFolderOpen} style={style} />,
-        //       leaf: "",
-        //     }}
-        //   />
-        // )
-      }
+      {attributes && (
+        <CheckboxTree
+          nodes={sortAttributes(attributes)}
+          onlyLeafCheckboxes={true}
+          checked={attChecked}
+          expanded={expanded}
+          onCheck={(checked) => setAttChecked(checked)}
+          onExpand={(expanded) => setExpanded(expanded)}
+          icons={{
+            check: (
+              <FontAwesomeIcon
+                icon={faSquareCheck}
+                style={{ color: "#709c62ff" }}
+              />
+            ),
+            uncheck: (
+              <FontAwesomeIcon icon={faSquare} style={{ color: "#709c62ff" }} />
+            ),
+            expandClose: (
+              <FontAwesomeIcon icon={faChevronRight} style={style} />
+            ),
+            expandOpen: <FontAwesomeIcon icon={faChevronDown} style={style} />,
+            expandAll: <FontAwesomeIcon icon={faPlusSquare} style={style} />,
+            collapseAll: <FontAwesomeIcon icon={faMinusSquare} style={style} />,
+            parentClose: <FontAwesomeIcon icon={faFolder} style={style} />,
+            parentOpen: <FontAwesomeIcon icon={faFolderOpen} style={style} />,
+            leaf: "",
+          }}
+        />
+      )}
     </div>
   );
-  // return attributes?.length && sortAttributes(attributes);
 }
