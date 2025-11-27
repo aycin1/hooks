9947b6a9e0d1275824a36a0aa65a9efe43870893
aycin <aycinkaya98@gmail.com>
2025-11-27 11:46:59 +0000

    amended base URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 export default axios.create({ baseURL: `${BASE_URL}/api` });
 
 export const axiosPrivate = axios.create({
-  baseURL: BASE_URL,
+  baseURL: `${BASE_URL}/api`,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
