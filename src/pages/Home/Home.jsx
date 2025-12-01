4c3d01eb8d77a5741b04ce1e96a840093c338e15
aycin <aycinkaya98@gmail.com>
2025-12-01 15:22:20 +0000

    added heading to homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
+import useUsername from "../../hooks/useUsername";
 import UserSearch from "./../../components/UserSearch/UserSearch";
 import Dashboard from "./components/Dashboard";
 import styles from "./Home.module.css";
 
 export default function Home() {
+  const username = useUsername();
+
   return (
     <div className={styles.home}>
+      <h2 className={styles.heading}>Welcome, {username}!</h2>
       <Dashboard />
 
       <div className={styles.userSearch}>
